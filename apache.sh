8fc1e13a443a5559e1135b3defbe788a271abed9
zxcV32 <i14a23h19a@gmail.com>
2020-04-09 23:36:30 +0530

    Create apache.sh

---
+++ apache.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Run as root
+
+# check permissions
+if [ "$(whoami)" != "root" ]; then
+        echo "Root privileges are required to run this, try running with sudo or with root user..."
+        exit 2
+fi
+
+if [ "$1" == "" ]; then
+  echo "Please specify a path."
+fi
+
+
+AP_ROOT=$1 # Apache2 root directory
+AP_OWNER=www-data #  Apachd2 owner
+AP_GROUP=www-data # <-- Apache2 group
+
+# reset to safe defaults
+find ${AP_ROOT} -exec chown ${AP_OWNER}:${AP_GROUP} {} \;
+find ${AP_ROOT} -type d -exec chmod 755 {} \;
+find ${AP_ROOT} -type f -exec chmod 644 {} \;
